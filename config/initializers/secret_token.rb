fb6cd14acf641558212eef08512d57225e00019a
Daisuke Ozaki <do.it.now420@gmail.com>
2013-05-30 06:42:43 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Salon::Application.config.secret_key_base = '5972369c6b9c5db8b837c2804f985e2bfa316295ddc851b81c3f294bfda03f69e97ea219de7c1cfcc46fdc506d239ad30e38db456ce91a06ac17a6ec917c59c9'
